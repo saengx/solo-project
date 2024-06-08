87e86b238781a537bacb2f93df82360f563f63f7
saeng <saengx@github.com>
2024-06-08 14:40:19 +0700

    first commit

---
+++ src-miner/config.py
@@ -0,0 +1,16 @@
+import os, json, requests
+
+
+def banner():
+    os.system("clear")
+    print("\033[1;32;40m")
+    os.system("figlet -f ANSI_Shadow solo")
+    os.system("figlet -f ANSI_Shadow miner")
+    print("\033[00m\n")
+
+def banneredit():
+    os.system("clear")
+    print("\033[1;32;34m")
+    os.system("figlet -f  ANSI_Shadow EDIT")
+    print("\033[00m\n")
+    print(f"\n\033[96mDeveloper by PICHET SAENGTEWAN\033[0m\n")
